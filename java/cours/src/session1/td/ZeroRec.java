7711db1dfd6d5943b8099fc6e4bb7e67d8f5174d
jgeneve <jgeneve74@gmail.com>
2018-10-07 17:36:19 +0200

    First commit
    First commit

--- java/cours/src/session1/td/ZeroRec.java
+++ java/cours/src/session1/td/ZeroRec.java
@@ -2,14 +2,14 @@ package session1.td;
 
 public class ZeroRec extends Zero implements Nat {
 	
-	public static FabriqueNaturels<Nat> FAB = new ZeroRec();
+	public static FabriqueNaturels<Nat> FAB = new NatParInt();
 
 	public ZeroRec() {}
 	
 	//Create the successor of an object
 	@Override
 	public Nat creerSuccesseur(Nat predecesseur) {
-		return creerNatAvecValeur(predecesseur.val()+1);
+		return FAB.creerNatAvecValeur(predecesseur.val()+1);
 	}
 
 	
@@ -22,7 +22,7 @@ public class ZeroRec extends Zero implements Nat {
 	//Return a neutral element of the addition, a.k.a zero
 	@Override
 	public Nat zero() {
-		return creerNatAvecValeur(0);
+		return FAB.creerNatAvecValeur(0);
 	}
 
 	//Multiplication between two numbers
@@ -34,19 +34,19 @@ public class ZeroRec extends Zero implements Nat {
 	//Return a neutral element of the multiplication, a.k.a one
 	@Override
 	public Nat un() {
-		return creerNatAvecValeur(1);
+		return FAB.creerNatAvecValeur(1);
 	}
 
 	//Return the rest of the Euclidean division this.val/x
 	@Override
 	public Nat modulo(Nat x) {
-		return (x.estNul()) ? null : creerNatAvecValeur(this.val()%x.val());
+		return (x.estNul()) ? null : FAB.creerNatAvecValeur(this.val()%x.val());
 	}
 
 	//Return the quotient of the division this.val/x
 	@Override
 	public Nat div(Nat x) {
-		return (x.estNul()) ? null : creerNatAvecValeur(this.val()/x.val());
+		return (x.estNul()) ? null : FAB.creerNatAvecValeur(this.val()/x.val());
 	}
 
 	//Equals method
